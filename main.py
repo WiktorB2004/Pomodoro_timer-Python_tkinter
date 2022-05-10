1821b7b195af6095b36bc1da235a9c4a72a1290a
Wiktor <byrka.w@gmail.com>
2022-05-10 21:03:29 +0200

    created first sketch of the application

---
+++ main.py
@@ -0,0 +1,26 @@
+# Imports
+from tkinter import *
+from tkinter import ttk
+
+
+# Window in which you can set amount of rounds and learning and break time
+def menu():
+    pass
+
+
+# Window in which you see countdown and rounds left
+def learn_timer():
+    pass
+
+
+# Window in which you see break time
+def break_timer():
+    pass
+
+
+# Countdown function
+def countdown(time):
+    pass
+
+
+# TODO: Create Window with todo list, summary and GUI with all components working together
